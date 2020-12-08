e573496b485aa5d988ae4b44a929c889f21827d3
thanhnhan <sieunhan1397@gmail.com>
2020-12-09 00:05:01 +0700

    hash password with salt

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,16 +1,33 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
-    const { username, password } = authCredentialsDto;
+    try {
+      const { username, password } = authCredentialsDto;
 
-    const user = new User();
-    user.username = username;
-    user.password = password;
+      const user = new User();
+      user.username = username;
+      user.salt = await bcrypt.getSalt();
+      user.password = await this.hashPassword(password, user.salt);
+      await user.save();
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
 
-    user.save();
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return await bcrypt.hash(password, salt);
   }
 }
